de86928980264bc5ff4fb03c53f88dec7451dce0
UsmanKhan172014 <46988838+fahad0909@users.noreply.github.com>
2023-01-12 17:53:22 +0500

    pptx added

--- templatedocs/views.py
+++ templatedocs/views.py
@@ -5,6 +5,7 @@ from .models import UploadFileForm
 import docx
 import os
 import re
+from pptx import Presentation
 
 
 # define the home view
@@ -40,40 +41,57 @@ def update_doc_form(request, id):
     # baseurl = request.build_absolute_uri('/')
     basePath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     file = basePath + "/media/" + str(data.file)
-    doc = docx.Document(data.file)
-    unique_placeholders = set()
-    for para in doc.paragraphs:
-        placeholders = re.findall(r"\{.*\}", para.text)
-        unique_placeholders.update(placeholders)
+    if ".docx" in file:
+        doc = docx.Document(data.file)
+        unique_placeholders = set()
+        for para in doc.paragraphs:
+            placeholders = re.findall(r"\{.*\}", para.text)
+            unique_placeholders.update(placeholders)
 
-    for table in doc.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                for para in cell.paragraphs:
-                    placeholders = re.findall(r"\{.*\}", para.text)
+        for table in doc.tables:
+            for row in table.rows:
+                for cell in row.cells:
+                    for para in cell.paragraphs:
+                        placeholders = re.findall(r"\{.*\}", para.text)
+                        unique_placeholders.update(placeholders)
+
+        unique_placeholders = list(unique_placeholders)
+        return render(request, 'templatedocs/updateDoc.html', {'id': id, 'unique_placeholders': unique_placeholders})
+    elif ".pptx" in file:
+        prs = Presentation(file)
+        unique_placeholders = set()
+        for slide in prs.slides:
+            for shape in slide.shapes:
+                if not shape.has_text_frame:
+                    continue
+                for paragraph in shape.text_frame.paragraphs:
+                    placeholders = re.findall(r"\{.*\}", paragraph.text)
                     unique_placeholders.update(placeholders)
-
-    unique_placeholders = list(unique_placeholders)
-    return render(request, 'templatedocs/updateDoc.html', {'id': id, 'unique_placeholders': unique_placeholders})
-
+        unique_placeholders = list(unique_placeholders)
+        # return HttpResponse(unique_placeholders)
+        return render(request, 'templatedocs/updateDoc.html', {'id': id, 'unique_placeholders': unique_placeholders})
+    else:
+        return HttpResponse("File not supported")
 
 def update(request, id):
     data = UploadFileForm.objects.get(id=id)
     if request.method == 'POST':
         base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        doc = docx.Document(data.file)
-        try:
-            for para in doc.paragraphs:
-                placeholders = re.findall(r"\{.*\}", para.text)
-                for placeholder in placeholders:
-                    style1 = doc.styles['heading 1']
-                    style2 = doc.styles['heading 2']
-                    default_color = style2.font.color.rgb
-                    if str(default_color) == 'FFFFFF':
-                        para.text = para.text.replace(placeholder, request.POST[placeholder])
-                        para.style.font.color.rgb = style1.font.color.rgb
-                    else:
-                        para.text = para.text.replace(placeholder, request.POST[placeholder])
+        if ".docx" in data.file.name:
+            doc = docx.Document(data.file)
+
+            try:
+                for para in doc.paragraphs:
+                    placeholders = re.findall(r"\{.*\}", para.text)
+                    for placeholder in placeholders:
+                        style1 = doc.styles['heading 1']
+                        style2 = doc.styles['heading 2']
+                        default_color = style2.font.color.rgb
+                        if str(default_color) == 'FFFFFF':
+                            para.text = para.text.replace(placeholder, request.POST[placeholder])
+                            para.style.font.color.rgb = style1.font.color.rgb
+                        else:
+                            para.text = para.text.replace(placeholder, request.POST[placeholder])
                 for table in doc.tables:
                     for row in table.rows:
                         for cell in row.cells:
@@ -83,10 +101,37 @@ def update(request, id):
                                     style = para.style
                                     para.text = para.text.replace(placeholder, request.POST[placeholder])
                                     para.style = style
-            doc.save(base_path + "/media/documents/updated.docx")
-            response = FileResponse(open(base_path + "/media/documents/updated.docx", 'rb'),
-                                    content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-            return response
-        except Exception as e:
-            print(e)
-            return HttpResponse(e)
+
+                doc.save(base_path + "/media/documents/updated.docx")
+                response = FileResponse(open(base_path + "/media/documents/updated.docx", 'rb'),
+                                        content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+                response['Content-Disposition'] = 'attachment; filename="Hello.docx"'
+                return response
+            except Exception as e:
+                print(e)
+                return HttpResponse(e)
+        elif ".pptx" in data.file.name:
+            prs = Presentation(base_path + "/media/" + str(data.file))
+            try:
+                for slide in prs.slides:
+                    for shape in slide.shapes:
+                        if not shape.has_text_frame:
+                            continue
+                        for paragraph in shape.text_frame.paragraphs:
+                            placeholders = re.findall(r"\{.*\}", paragraph.text)
+                            for placeholder in placeholders:
+                                print(placeholder)
+                                paragraph.text = paragraph.text.replace(placeholder, request.POST[placeholder])
+                prs.save(base_path + "/media/documents/updated.pptx")
+                response = FileResponse(open(base_path + "/media/documents/updated.pptx", 'rb'),
+                                        content_type='application/vnd.openxmlformats-officedocument.presentationml.presentation')
+                response['Content-Disposition'] = 'attachment; filename="Hello.pptx"'
+                return response
+                # return HttpResponse("Success")
+            except Exception as e:
+                print(e)
+                return HttpResponse("Error")
+        else:
+            return HttpResponse("File not supported")
+    else:
+        return HttpResponseRedirect('/templatedocs/home/')
