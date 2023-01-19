ea35535312801f683bdd6885a55a8a3781ebbe30
UsmanKhan172014 <46988838+fahad0909@users.noreply.github.com>
2023-01-19 17:47:47 +0500

    basics

--- templatedocs/urls.py
+++ templatedocs/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload_file/', views.upload_file, name='upload_file'),
-    path('home/', views.index, name='index'),
+    path('upload_file/', views.home, name='upload_file'),
+    path('get_all/', views.get_all, name='get_all'),
     path('delete/<int:id>', views.delete, name='delete'),
-    path('update_doc_form/<int:id>', views.update_doc_form, name='updateDoc'),
+    path('updateDoc/<int:id>', views.updateDoc, name='updateDoc'),
     path('update/<int:id>', views.update, name='update'),
+    path('load_admin/', views.load_admin, name='load_admin'),
 ]
