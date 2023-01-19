7afdfd3321ee6acb39d6b122b51816d597d81b02
UsmanKhan172014 <46988838+fahad0909@users.noreply.github.com>
2023-01-19 17:55:37 +0500

    model updated

--- templatedocs/models.py
+++ templatedocs/models.py
@@ -6,3 +6,4 @@ class UploadFileForm(models.Model):
     description = models.CharField(max_length=255, blank=True)
     file = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    user_id = models.IntegerField(default=0)
